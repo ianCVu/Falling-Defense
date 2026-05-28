0eb76d1f9593ff31af85f65b613c1719bd6caba6
Ian Vu <ianv6207@mytusd.org>
2026-05-28 09:47:27 -0700

    Create header.js

---
+++ assets/header.js
@@ -0,0 +1,8 @@
+const header = document.createElement("div");
+header.id = "header";
+const headerHB = document.createElement("a");
+headerHB.className = "headerLink";
+headerHB.href = "/index.html";
+headerHB.textContent = "Falling Defense";
+document.body.appendChild(header);
+header.appendChild(headerHB);
